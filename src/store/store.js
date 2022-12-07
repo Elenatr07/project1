fb453b04afa4b47f46a7a4caf46d38aa7906351f
Elenatr07 <e.turkina2010@yandex.ru>
2022-12-08 04:16:15 +1000

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -4,3 +4,8 @@ import initReducers from './reducers';
 export default function initStore() {
     return createStore(initReducers, {});
 }
+const store = createStore(
+    initReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+);
