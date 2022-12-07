83e9a9d85103c7c95d5615e6f9d694313f6b5450
Elenatr07 <e.turkina2010@yandex.ru>
2022-12-07 12:25:07 +1000

    1

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,17 @@
-import { createStore } from 'redux';
+import { legacy_createStore as createStore, compose, applyMiddleware } from 'redux';
 import initReducers from './reducers';
+import middelewares from '../middelewares';
+
 
 export default function initStore() {
-    return createStore(initReducers, {});
+    return createStore(initReducers, {}, compose(
+        applyMiddleware(...middelewares)
+    ));
 }
+//export default function initStore() {
+    //return createStore(initReducers, {}, compose(
+     //   applyMiddleware(...middelewares),
+     //   window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : () => { }
+    //));
+//}
+
